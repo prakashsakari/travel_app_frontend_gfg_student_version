d6a386f4193dd3f16f7369cbaf8e625f6bf2dedf
Prakash Sakari <sakariprakash@gmail.com>
2022-12-15 18:59:25 +0530

    updated categories with react carousel package

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,23 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useCategory, useFilter } from "../../context";
-
+import Carousel from 'react-elastic-carousel';
+import Item from "../Item/Item";
 import "./Categories.css";
 
 export const Categories = () => {
   const [categories, setCategories] = useState([]);
-  const [numberOfCategoryToShow, setNumberOfCategoryToShow] = useState(0);
   const { hotelCategory, setHotelCategory } = useCategory();
 
   const { filterDispatch } = useFilter();
-
-  const handleShowMoreRightClick = () => {
-    setNumberOfCategoryToShow((prev) => prev + 10);
-  };
-
-  const handleShowMoreLeftClick = () => {
-    setNumberOfCategoryToShow((prev) => prev - 10);
-  };
 
   const handleFilterClick = () => {
     filterDispatch({
@@ -31,53 +23,24 @@ export const Categories = () => {
         const { data } = await axios.get(
           "https://travelapp.cyclic.app/api/category"
         );
-        const categoriesToShow = data.slice(
-          numberOfCategoryToShow + 10 > data.length
-            ? data.length - 10
-            : numberOfCategoryToShow,
-          numberOfCategoryToShow > data.length
-            ? data.length
-            : numberOfCategoryToShow + 10
-        );
-        setCategories(categoriesToShow);
+        setCategories(data);
       } catch (err) {
         console.log(err);
       }
     })();
-  }, [numberOfCategoryToShow]);
+  }, []);
 
   const handleCategoryClick = (category) => {
     setHotelCategory(category);
   };
 
   return (
-    <section className="categories d-flex align-center gap-large cursor-pointer">
-      {numberOfCategoryToShow >= 10 && (
-        <button
-          className="button btn-category btn-left fixed cursor-pointer"
-          onClick={handleShowMoreLeftClick}
-        >
-          <span class="material-icons-outlined">chevron_left</span>
-        </button>
-      )}
-      {categories &&
-        categories.map(({ _id, category }) => (
-          <span
-            className={`${category === hotelCategory ? "border-bottom" : ""}`}
-            key={_id}
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category}
-          </span>
-        ))}
-      {numberOfCategoryToShow - 10 < categories.length && (
-        <button
-          className="button btn-category btn-right fixed cursor-pointer"
-          onClick={handleShowMoreRightClick}
-        >
-          <span class="material-icons-outlined">chevron_right</span>
-        </button>
-      )}
+    <section className="categories">
+      <Carousel itemsToShow={9} itemsToScroll={9} pagination={false}>
+        {
+          categories && categories.map(({_id, category}) => <Item className={`${category === hotelCategory ? "border-bottom" : ""}`} onClick={() => handleCategoryClick(category)} key={_id}>{category}</Item>)
+        }
+      </Carousel>
       <button
         className="button btn-filter d-flex align-center gap-small cursor-pointer fixed"
         onClick={handleFilterClick}
---
+++ src/components/Item/Item.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: max-content;
+  font-size: .875rem;
+  cursor: pointer;
+`;
